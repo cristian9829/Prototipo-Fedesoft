080024b147bf42c0babc41483be94b06fa84cd0d
cristian clavijo <cristian982940@gmail.com>
2018-10-10 23:33:42 -0500

    Version 10/10/2018
    Se agregaron mas imagenes al proyecto
    
    En esta version, se desarrollo el javascript correspondiente cuando en determinado tiempo la imagen se cambie a otra aleatoriamente
    
    Ademas se agrego una pequeña validacion del primer formulario mostrando un pequeño mensaje dependiendo de la ocacion

--- Prototipo/js/index.js
+++ Prototipo/js/index.js
@@ -1,14 +1,37 @@
 function btnForms (formularioHide,formularioShow){
-	$(formularioHide).hide();
-	$(formularioShow).show();
+	var inputs = $(formularioHide + " input")
+	if (!$(inputs).val()) {
+		$(".advertencia").css("display","block");	     
+		return false;
+	      
+	    }else{
+	    	$(".advertencia").text("Perfecto");
+	    	$(".advertencia").css("color","green");
+
+			$(formularioHide).hide(800);
+			$(formularioShow).show(200);
+	    }
 }
 
 function btnForms2 (formularioShow,formularioHide){
-	$(formularioShow).show();
-	$(formularioHide).hide();
+	$(formularioShow).show(200);
+	$(formularioHide).hide(500);
 }
 
+function imagenRandom(){
+	var random = Math.floor(Math.random() * 9);
+	$(".contenedorImagen").fadeOut(500,function(){
+		$(this).css("background-image","url('Imagenes/fondo" +random+".jpg')").fadeIn(1000);
+		console.log(random);				
+	})
+}
+
+
+
 $(document).ready(function(){ /*Codigo jquery*/
+	
+	setInterval(imagenRandom,9000);
+
 	$("#btn-documento-siguiente").on("click",function(e){
 		e.preventDefault();
 		btnForms(".contenedorFormulario form",".contenedorFormulario #formulario2");
@@ -17,5 +40,6 @@ $(document).ready(function(){ /*Codigo jquery*/
 	$("#btn-documento-atras").on("click",function(e){
 		e.preventDefault();
 		btnForms2(".contenedorFormulario form",".contenedorFormulario #formulario2");
-	})
+	})	
+
 })
